576f605a2a56037636797ddcc7e213dc916ec7bf
dark <mail@mathiasjost.com>
2012-09-13 01:32:14 +0200

    initial commit
    initial commit of the addon files.

---
+++ eDisturbed.lua
@@ -0,0 +1,38 @@
+-- =============================================================================
+-- 
+--       Filename:  eDisturbed.lua
+-- 
+--    Description:  Only shows the first DND and AFK message 
+--                  and hides the ones following.
+-- 
+--        Version:  5.0.1
+--         Author:  Mathias Jost (mail@mathiasjost.com)
+-- 
+-- =============================================================================
+
+
+-- -----------------------------------------------------------------------------
+-- Local array holding a list of players. Will not be saved throuout reloads.
+-- -----------------------------------------------------------------------------
+local data = {}
+
+-- -----------------------------------------------------------------------------
+-- Handels the events
+-- -----------------------------------------------------------------------------
+local function eDisturbed(_, _, _, sender)
+
+	if data[sender] == true then
+		return true
+	else
+		data[sender] = true
+		return false
+	end -- if data[sender] == true then
+	
+end -- function eDisturbed(_, _, _, sender)
+
+
+-- -----------------------------------------------------------------------------
+-- Registers the events with the DND Filter
+-- -----------------------------------------------------------------------------
+ChatFrame_AddMessageEventFilter("CHAT_MSG_AFK", eDisturbed)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_DND", eDisturbed)
